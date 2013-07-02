be0755ef513b8fbbbe7e5bd8d10b80e9a6853616
KevinSotoV <kasv2011@hotmail.com>
2013-07-01 22:00:01 -0500

    Update editor-textarea.php

--- upload/static/templates/system/blocks/editor-textarea.php
+++ upload/static/templates/system/blocks/editor-textarea.php
@@ -1,6 +1,6 @@
 <div class="user-status-field htmlarea">
 	<div class="textarea-wrap">
-		<textarea name="message" tabindex="1" data-placeholder="Share something {%in_group%}..."><?= $this->page->lang('activity_text_box_shate_txt') ?> {%in_group%}...</textarea>
+		<textarea name="message" tabindex="1" data-placeholder="<?= $this->page->lang('activity_text_box_shate_txt') ?> {%in_group%}..."><?= $this->page->lang('activity_text_box_shate_txt') ?> {%in_group%}...</textarea>
 		<div class="textarea-highlighter"><span></span></div>
 	</div>
 </div>
